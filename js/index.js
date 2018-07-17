cb2e28024b966dcfcbf1eb31471c13a7a20e5333
Artem Lebedev <artemlebed1995@gmail.com>
2018-07-17 16:10:55 +0000

    not work annimation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.querySelector('.sections')
-	.addEventListener('click', imageshow);
+     .forEach( el => el.addEventListener('click', imageshow) )
+	
 function imageshow() {
 	document.querySelector('.images').style.animation = 'images 1s 1 forwards';
 	document.querySelector('.music').style.animation = 'gomusic 1s 1 forwards linear';
