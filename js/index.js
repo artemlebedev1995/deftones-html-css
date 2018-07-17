3263bcffb7e838d8d0dc7dce70c952bae8d30b6b
Artem Lebedev <artemlebed1995@gmail.com>
2018-07-17 21:32:18 +0000

    annimation done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,66 @@
-document.querySelector('.sections')
-     .forEach( el => el.addEventListener('click', imageshow) )
+var counter;
+document.querySelector('.adrenalineClick')
+     .addEventListener('click', adrenaline);
+     
 	
-function imageshow() {
-	document.querySelector('.images').style.animation = 'images 1s 1 forwards';
-	document.querySelector('.music').style.animation = 'gomusic 1s 1 forwards linear';
-	document.querySelector('.music').style.display = 'block';
+function adrenaline() {
+     document.querySelector('.adrenalineimage').style.animation = 'images 1s 1 forwards';
+	document.querySelector('.adrenaline').style.animation = 'gomusic 1s 1 forwards linear';
+	document.querySelector('.adrenaline').style.display = 'block';
+}
+document.querySelector('.arroundthefurClick')
+     .addEventListener('click', arroundthefur); 
+
+function arroundthefur() {
+     document.querySelector('.arroundthefurimage').style.animation = 'images 1s 1 forwards';
+	document.querySelector('.arroundthefur').style.animation = 'gomusic 1s 1 forwards linear';
+	document.querySelector('.arroundthefur').style.display = 'block';
+}    
+document.querySelector('.whiteponyClick')
+     .addEventListener('click', whitepony ); 
+
+function whitepony() {
+     document.querySelector('.whiteponyimage').style.animation = 'images 1s 1 forwards';
+	document.querySelector('.whitepony').style.animation = 'gomusic 1s 1 forwards linear';
+	document.querySelector('.whitepony').style.display = 'block';
+}
+document.querySelector('.deftonesClick')
+     .addEventListener('click', deftones ); 
+
+function deftones () {
+     document.querySelector('.deftonesimage').style.animation = 'images 1s 1 forwards';
+	document.querySelector('.deftones').style.animation = 'gomusic 1s 1 forwards linear';
+	document.querySelector('.deftones').style.display = 'block';
+}
+document.querySelector('.saturdayClick')
+     .addEventListener('click', saturday ); 
+
+function saturday () {
+     document.querySelector('.saturdayimage').style.animation = 'images 1s 1 forwards';
+	document.querySelector('.saturday').style.animation = 'gomusic 1s 1 forwards linear';
+	document.querySelector('.saturday').style.display = 'block';
+}
+document.querySelector('.diamondClick')
+     .addEventListener('click', diamond ); 
+
+function diamond () {
+     document.querySelector('.diamondimage').style.animation = 'images 1s 1 forwards';
+	document.querySelector('.diamond').style.animation = 'gomusic 1s 1 forwards linear';
+	document.querySelector('.diamond').style.display = 'block';
+}
+document.querySelector('.koiClick')
+     .addEventListener('click', koi ); 
+
+function koi () {
+     document.querySelector('.koiimage').style.animation = 'images 1s 1 forwards';
+	document.querySelector('.koi').style.animation = 'gomusic 1s 1 forwards linear';
+	document.querySelector('.koi').style.display = 'block';
+}
+document.querySelector('.goreClick')
+     .addEventListener('click', gore ); 
+
+function  gore () {
+     document.querySelector('.goreimage').style.animation = 'images 1s 1 forwards';
+	document.querySelector('.gore').style.animation = 'gomusic 1s 1 forwards linear';
+	document.querySelector('.gore').style.display = 'block';
 }
